9d8ea3d90407bb07c594630a10a1d430c3507e38
danel <danelign@yandex.ru>
2020-09-23 15:34:09 +0400

    Оставил 1 топик для логера, вместо двух

--- logger/src/main/scala/payments/logger/PaymentLoggingEgress.scala
+++ logger/src/main/scala/payments/logger/PaymentLoggingEgress.scala
@@ -7,14 +7,12 @@ import cloudflow.streamlets.avro._
 import payments.datamodel._
 
 class PaymentLoggingEgress extends AkkaStreamlet {
-  val inChecking            = AvroInlet[LogMessage]("in-checking")
-  val inProcessor           = AvroInlet[LogMessage]("in-processor")
-  val shape: StreamletShape = StreamletShape.withInlets(inChecking, inProcessor)
+  val in                    = AvroInlet[LogMessage]("in")
+  val shape: StreamletShape = StreamletShape(in)
 
   override protected def createLogic(): AkkaStreamletLogic = new AkkaStreamletLogic() {
     override def run(): Unit =
-      Merger
-        .source(inChecking, inProcessor)
+      sourceWithCommittableContext(in)
         .map {
           case LogMessage(reason, message, "info") => system.log.info(s"$reason: $message")
           case LogMessage(reason, message, "warn") => system.log.warning(s"$reason: $message")
